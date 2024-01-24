8701aa4074a4fd615acdf71bd430520e2a837227
golubkovan <153586009+golubkovan@users.noreply.github.com>
2024-01-24 16:38:21 +0400

    1

--- terraform1/main.tf
+++ terraform1/main.tf
@@ -3,31 +3,7 @@ locals {
     bucket_name = "tf-intro-site-bucket-agolubkov"
 }
 
-// Create SA
-resource "yandex_iam_service_account" "sa" {
-  folder_id = local.folder_id
-  name      = "tf-test-sa"
-}
 
-// Grant permissions
-resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
-  folder_id = local.folder_id
-  role      = "storage.admin"
-  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
-}
-
-// Create Static Access Keys
-resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
-  service_account_id = yandex_iam_service_account.sa.id
-  description        = "static access key for object storage"
-}
-
-// Use keys to create bucket
-resource "yandex_storage_bucket" "test" {
-  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-  bucket = local.bucket_name
-}
 
 //------------Create BuildVM---------------------//
 # ресурс "yandex_compute_instance" т.е. сервер
@@ -44,7 +20,7 @@ resource "yandex_compute_instance" "default" {
 
   boot_disk {
     initialize_params {
-      image_id = "fd8v0s6adqu3ui3rsuap" # ОС (Ubuntu, 22.04 LTS)
+      image_id = "e9besrroes8vu5s5ce0a" # ОС (Ubuntu, 22.04 LTS)
     }
   }
 
