c3dc8abc0aa4d864bcdc7e98fcc017ff3d0d3637
golubkovan <153586009+golubkovan@users.noreply.github.com>
2024-01-25 15:47:19 +0400

    PROD VM

--- terraform1/main.tf
+++ terraform1/main.tf
@@ -35,7 +35,7 @@ connection {
     private_key = file("~/.ssh/id_rsa")
     host = yandex_compute_instance.build-vm.network_interface.0.nat_ip_address
   }
-#-------/Connect to Vm build---------#
+#-------/Build & push docker image---------#
 
   provisioner "file" {
     source      = "dockerfile"
@@ -56,4 +56,45 @@ connection {
 
     ]
   }
+}
+
+#----------PROD VM-------------#
+resource "yandex_compute_instance" "prod-vm" {
+  name = "prod-vm"
+  allow_stopping_for_update = true
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd88m3uah9t47loeseir" # ОС (Ubuntu, 22.04 LTS)
+    }
+  }
+
+  network_interface {
+    subnet_id = "e9besrroes8vu5s5ce0a" # одна из дефолтных подсетей
+    nat = true # автоматически установить динамический ip
+  }
+  metadata = {
+    user-data = "${file("/mnt/c/Users/golubkovan/VsCode/terraformHW/terraform1/meta.txt")}"
+  }
+#-------Connect to Vm prod---------#
+  connection {
+      type     = "ssh"
+      user     = "agolubkov"
+      private_key = file("~/.ssh/id_rsa")
+      host = yandex_compute_instance.prod-vm.network_interface.0.nat_ip_address
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update", 
+      "sudo apt install docker.io -y",
+      "sudo docker pull cr.yandex/${yandex_container_registry.agolubkovreg.id}/tomcat_box",
+      "sudo docker run -d -p 8080:8080 cr.yandex/${yandex_container_registry.agolubkovreg.id}/tomcat_box"
+    ]
+  }
+
+
 }
