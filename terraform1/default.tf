96214e0d022604294a5b07ca9c02db8c10a6f8e7
golubkovan <153586009+golubkovan@users.noreply.github.com>
2024-01-25 15:18:29 +0400

    1

--- terraform1/default.tf
+++ terraform1/default.tf
@@ -18,3 +18,25 @@ provider "yandex" {
   folder_id                = local.folder_id
   zone                     = "ru-central1-a"
 }
+
+resource "yandex_container_registry" "agolubkovreg" {
+  name = "agolubkovreg"
+  folder_id = local.folder_id
+  labels = {
+    my-label = "agolubkovreg1"
+  }
+}
+resource "yandex_container_registry_iam_binding" "puller" {
+  registry_id = yandex_container_registry.agolubkovreg.id
+  role        = "container-registry.images.puller"
+  members = [
+    "system:allUsers",
+  ]
+}
+resource "yandex_container_registry_iam_binding" "pusher" {
+  registry_id = yandex_container_registry.agolubkovreg.id
+  role        = "container-registry.images.pusher"
+  members = [
+    "system:allUsers",
+  ]
+}
